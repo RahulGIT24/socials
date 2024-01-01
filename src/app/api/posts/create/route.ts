d658fd93856abf4da6d0bc073d671f4a6c9d8bcd
rahul <rg4005450@gmail.com>
2024-01-01 14:53:00 +0530

    Make some changes in login and signup route and removed a comment in a route

--- src/app/api/posts/create/route.ts
+++ src/app/api/posts/create/route.ts
@@ -11,7 +11,6 @@ interface RequestBody {
     pic: string;
 }
 
-// TODO
 export async function POST(request: NextRequest) {
     try {
         connect();
--- src/app/api/users/auth/signup/route.ts
+++ src/app/api/users/auth/signup/route.ts
@@ -56,6 +56,7 @@ export async function POST(request: NextRequest) {
         const response = NextResponse.json({
             message: "Welcome to Socials",
             token,
+            expires: new Date(Date.now() + 60 * 24 * 60 * 60 * 1000), // 60 days in milliseconds
             success: true
         })
 
